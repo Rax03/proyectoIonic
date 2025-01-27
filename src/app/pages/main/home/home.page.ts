d13bbb3a9b5caf7805db44a26d3157ef1395d029
Alfonso Jiménez <yo@alfonsojimenez.net>
2025-01-27 08:04:05 +0100

    Punto 21 terminado

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, MinLengthValidator } from '@angular/forms';
-import { IonContent, IonButton, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonFab, IonFabButton, IonIcon, IonLabel, IonItem, IonItemSliding, IonList, IonItemOptions, IonItemOption, IonAvatar, IonChip } from '@ionic/angular/standalone';
 import { UtilsService } from 'src/app/services/utils.service';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { HeaderComponent } from "../../../shared/components/header/header.component";
 import { addIcons } from 'ionicons';
-import { add } from 'ionicons/icons';
+import { add, createOutline, trashOutline } from 'ionicons/icons';
 import { AddUpdateMiniatureComponent } from 'src/app/shared/components/add-update-miniature/add-update-miniature.component';
 import { Miniature } from 'src/app/models/miniature.model';
 
@@ -15,36 +15,42 @@ import { Miniature } from 'src/app/models/miniature.model';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonFabButton, IonFab, IonButton, IonContent, CommonModule, FormsModule, HeaderComponent]
+  imports: [IonChip, IonAvatar, IonItemOption, IonItemOptions, IonList, IonItemSliding, IonItem, IonLabel, IonIcon, IonFabButton, IonFab, IonButton, IonContent, CommonModule, FormsModule, HeaderComponent]
 })
 export class HomePage implements OnInit {
   utilsService = inject(UtilsService);
   firebaseService = inject(FirebaseService)
-  miniatures: Miniature[];
-  constructor() { addIcons({add}); }
+  miniatures: Miniature[] = [];
+  constructor() { addIcons({createOutline,trashOutline,add}); }
 
   ngOnInit() {
   }
 
-  signOut() {
-    this.firebaseService.signOut().then(() => {
-      this.utilsService.routerLink('/auth');
-    });
-  }
+  // signOut() {
+  //   this.firebaseService.signOut().then(() => {
+  //     this.utilsService.routerLink('/auth');
+  //   });
+  // }
 
-  getMiniatures() {
+  async getMiniatures() {
     const user = this.utilsService.getLocalStorageUser();
     const path: string = `users/${user.uid}/miniatures`;
 
-    let sub = this.firebaseService.getCollectionData(path).subscribe({
-      next: (res => any) {
+    let sub = (await this.firebaseService.getCollectionData(path)).subscribe({
+      next: (res: any) => {
         sub.unsubscribe();
+
         this.miniatures = res;
-      }
+      },
     });
   }
 
   addUpdateMiniature() {
     this.utilsService.presentModal({component: AddUpdateMiniatureComponent, cssClass: "add-update-modal"})
   }
+  
+  ionViewWillEnter() {
+    this.getMiniatures();
+  }
+  
 }
